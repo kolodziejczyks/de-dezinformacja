7a4b9d46b42e3e513d2f25a5e73a020b4ef514e4
Szymon Kołodziejczyk <szy.kolodziejczyk@gmail.com>
2025-11-10 11:53:46 +0100

    feat: pl/en translations

--- src/app/news-search/news-search.ts
+++ src/app/news-search/news-search.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { MatInput, MatFormField, MatLabel, MatError, MatSuffix } from '@angular/material/input';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-news-search',
@@ -13,8 +14,9 @@ import { MatInput, MatFormField, MatLabel, MatError, MatSuffix } from '@angular/
     ReactiveFormsModule,
     MatIcon,
     MatIconModule,
-    MatSuffix
-],
+    MatSuffix,
+    TranslatePipe,
+  ],
   templateUrl: './news-search.html',
   styleUrl: './news-search.scss',
 })
